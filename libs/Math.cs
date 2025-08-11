3f4f20bce60b8d23047f4e43c49f25db50807b78
Veïla <dabluefox.virality@gmail.com>
2025-08-11 08:39:27 +0200

    Create Math.cs

---
+++ libs/Math.cs
@@ -0,0 +1,17 @@
+
+
+namespace Veila.libs;
+
+public static class Math
+{
+  /// <summary>
+  /// Calculates an average over time without storing all previous samples.
+  /// Only works for sid > 1.
+  /// </summary>
+  /// <param name="average">The current accumulated average</param>
+  /// <param name="newValue">The value to add to the average calculation</param>
+  /// <param name="sid">The "id" of the new value</param>
+  /// <returns>The new average calculated</returns>
+  public static float AccumulatedAverage(float average, float newValue, float sid)
+    =>  average * (sid-1)/sid + newValue/sid;
+}
